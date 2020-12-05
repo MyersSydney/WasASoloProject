a3ecc4c8ad6c529e469def5a01f1d00cb5a6668c
squidmyers33 <squidmyers33@gmail.com>
2020-12-04 21:00:02 -0600

    initial comit

---
+++ Reindeer/Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = new Vector3(0, Input.mousePosition.y);
+        transform.position = mousePosition;
+    }
+}
---
+++ Reindeer/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = new Vector3(0, Input.mousePosition.y, -10);
+        transform.position = mousePosition;
+    }
+}
