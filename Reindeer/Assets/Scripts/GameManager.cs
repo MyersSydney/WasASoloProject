ed68dbb7582731de86c50efa3457b33d12422bc5
Flare26 <47835828+Flare26@users.noreply.github.com>
2020-12-04 22:28:25 -0600

    oof

---
+++ Reindeer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float sceneTime = 0f;
+    GameObject player;
+    public int pretime;
+    public bool isPlaying = false;
+    public int distance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+        sceneTime -= pretime;
+    }
+
+    void StartGame()
+    {
+
+        //Function starts the match!
+        Debug.Log("!   Game Start  !");
+        isPlaying = true;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        sceneTime += Time.deltaTime;
+        if (sceneTime >= 0 && isPlaying == false)
+            StartGame();
+    }
+}
